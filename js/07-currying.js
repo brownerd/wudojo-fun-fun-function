8c7da2b9b6894180a77806dbe49b67750e1ed249
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-12-04 09:08:13 -0600

    curry lesson

--- js/07-currying.js
+++ js/07-currying.js
@@ -1,2 +1,46 @@
 // Currying - Part 6 of Functional Programming in JavaScript
 // https://www.youtube.com/watch?v=iZLP4qOwY8I
+
+// Currying is when a functions doesn't use all of its arguments up front.
+// Instead, the function wants to take the first argument and return a function that requires you to use the second argument and so on.
+
+let dragon = (name, size, element) =>
+  `${name} is a ${size} dragon that breathes ${element}!`
+
+console.log(dragon('jim', 'xl', 'fire'))
+
+// Curried example
+let dragonz =
+  name =>
+    size =>
+      element =>
+        `${name} is a ${size} dragon that breathes ${element}!`
+
+console.log(dragonz('larry')('xxxl')('lasers'))
+
+// The idea with Currying is that
+// your function can pass through your application
+// and gradually recieve the arguments that it needs
+
+let bozo = dragonz('bozo')
+let tiny = bozo('tiny')
+
+console.log(tiny('poo'))
+
+// Here is another example that isn't curried
+// but we are going to curry it
+
+let drags = [
+  {name: 'fizz', element: 'lasers'},
+  {name: 'puffy', element: 'milk'},
+  {name: 'zap', element: 'lasers'},
+  {name: 'scoob', element: 'dancing'}
+]
+
+let hasElement =
+  (element, obj) => obj.element === element
+
+let laserDrags =
+  drags.filter(x => hasElement('lasers', x))
+
+console.log(JSON.stringify({laserDrags}, null, 2))
